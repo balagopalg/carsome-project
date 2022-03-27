b16dfeb02780eb871db69409b18b85133edab02c
balagopalg <balagopal93@gmail.com>
2022-03-28 02:05:01 +0800

    cors policy

--- backend/index.ts
+++ backend/index.ts
@@ -10,6 +10,12 @@ app.use(
   })
 );
 
+app.all('/', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+});
+
 app.use('/',route);
 
 
